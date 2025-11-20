ccce0b476da768d01cd5d0371885d2b5324087f9
illicchpv <illich58@mail.ru>
2025-11-20 16:22:42 +0300

    закрытие меню фильтра при клике вне его.

--- app/ui/filter.tsx
+++ app/ui/filter.tsx
@@ -1,13 +1,32 @@
 'use client'
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Filter() {
   const [isOpen, setIsOpen] = useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
+  const catalogRef = useRef<HTMLDivElement>(null);
+
+  // Обработчик клика вне компонента, для закрытия меню
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!catalogRef.current) return;
+      if (!catalogRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) { // Если меню открыто, добавляем обработчик клика на весь документ
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => { // Если меню закрыто, удаляем обработчик клика
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
 
   const updateFilter = (value: string) => {
     const params = new URLSearchParams(searchParams);
@@ -17,13 +36,15 @@ export default function Filter() {
       params.delete('category');
     }
     router.replace(`${pathname}?${params.toString()}`);
+    setIsOpen(false); // Закрываем меню после выбора
   };
 
   return (
-    <div>
+    <div ref={catalogRef}>
       <div className="catalog-button">
         <button onClick={() => setIsOpen(v => !v)}>
-          <span className="catalog-button_burger"></span><span className="catalog-button_text">Каталог</span>
+          <span className="catalog-button_burger"></span>
+          <span className="catalog-button_text">Каталог</span>
         </button>
 
         <div className="catalog" style={{ display: isOpen ? 'block' : 'none' }}>
